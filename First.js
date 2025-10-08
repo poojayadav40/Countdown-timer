948cedbc4a3e409f4efc4da7e4f44ea77cf8dbca
poojayadav40 <poojaya40@gmail.com>
2025-10-08 17:01:53 +0530

    files added

--- First.js
+++ First.js
@@ -49,7 +49,7 @@ const timerFunction = () => {
     minutesElement.innerText = leftMinute;
     secondsElement.innerText = leftSecond;
 
-    // Stop Set Interval after reaching the target timee
+    // Stop Set Interval after a reaching the target timee
     if (difference < 0) {
       counterTimer.style.display = "None";
       heading.innerText = "Time is Up";
